391e47c767f69f2c5668306fabd49cdf8fd12e61
Tosotada <32612999+Tosotada@users.noreply.github.com>
2018-11-16 21:53:00 +0100

    Revert "Update (#1)" (#2)
    This reverts commit 652ace1fadd6617571b45fed3a9dcbc872f1449b.

--- config/secrets.js
+++ config/secrets.js
@@ -38,16 +38,10 @@ module.exports = {
     clientID: process.env.LINKEDIN_ID,
     clientSecret: process.env.LINKEDIN_SECRET,
     callbackURL: '/auth/linkedin/callback',
-    profileFields: ['public-profile-url'],
     scope: ['r_basicprofile', 'r_emailaddress'],
     passReqToCallback: true
   },
   slackHook: process.env.SLACK_WEBHOOK,
 
-  cookieSecret: process.env.COOKIE_SECRET,
-
-  stripe: {
-    public: process.env.STRIPE_PUBLIC,
-    secret: process.env.STRIPE_SECRET
-  }
+  cookieSecret: process.env.COOKIE_SECRET
 };
